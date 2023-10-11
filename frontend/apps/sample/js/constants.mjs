3e6d42bab8cf3c6919499e60a13df08cf9a16417
lalithaSurampudi <145753189+lalithaSurampudi@users.noreply.github.com>
2023-10-11 11:07:23 +0530

    Create constants.mjs

---
+++ frontend/apps/sample/js/constants.mjs
@@ -0,0 +1,39 @@
+/* 
+ * (C) 2020 TekMonks. All rights reserved.
+ * License: MIT - see enclosed license.txt file.
+ */
+const FRONTEND = "http://localhost:8080";
+const BACKEND = "http://localhost:9090";
+const APP_NAME = "LMS";
+const APP_PATH = `${FRONTEND}/apps/${APP_NAME}`;
+
+export const APP_CONSTANTS = {
+    FRONTEND, BACKEND, APP_PATH, APP_NAME,
+    LOGIN_HTML: APP_PATH + "/login.html",
+    DETAIL_HTML: APP_PATH + "/detail.html",
+    ADDBOOK_HTML: APP_PATH + "/addbook.html",
+    DELBOOK_HTML: APP_PATH + "/delbook.html",
+
+
+    SESSION_NOTE_ID: "com_monkshu_ts",
+
+    API_LOGIN: `${BACKEND}/apis/login`,
+    API_DETAIL: `${BACKEND}/apis/get_book`,
+    API_ADDBOOK: `${BACKEND}/apis/insert_book`,
+    API_DELBOOK: `${BACKEND}/apis/delete_book`, 
+    API_TRANSACTION: `${BACKEND}/apis/transactions`,
+    API_AUTH: `${BACKEND}/apis/authentication`,
+ 
+ 
+
+
+    USERID: "id",
+    USER_ROLE: "user",
+    GUEST_ROLE: "guest",
+    PERMISSIONS_MAP: {
+        user: [],
+        guest: [APP_PATH + "/login.html",APP_PATH + "/detail.html", $$.MONKSHU_CONSTANTS.ERROR_THTML]
+    },
+    API_KEYS: { "*": "uiTmv5YBOZMqdTb0gekD40PnoxtB9Q0k" },
+    KEY_HEADER: "X-API-Key"
+}
