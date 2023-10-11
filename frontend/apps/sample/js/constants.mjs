a5cea6a429c250af977e7e0072716833fd91195f
lalithaSurampudi <145753189+lalithaSurampudi@users.noreply.github.com>
2023-10-11 11:15:13 +0530

    Add files via upload

--- frontend/apps/sample/js/constants.mjs
+++ frontend/apps/sample/js/constants.mjs
@@ -9,6 +9,7 @@ const APP_PATH = `${FRONTEND}/apps/${APP_NAME}`;
 
 export const APP_CONSTANTS = {
     FRONTEND, BACKEND, APP_PATH, APP_NAME,
+    SIGNUP_HTML: APP_PATH + "/signup.html",
     LOGIN_HTML: APP_PATH + "/login.html",
     DETAIL_HTML: APP_PATH + "/detail.html",
     ADDBOOK_HTML: APP_PATH + "/addbook.html",
@@ -17,13 +18,12 @@ export const APP_CONSTANTS = {
 
     SESSION_NOTE_ID: "com_monkshu_ts",
 
+    API_SIGNUP: `${BACKEND}/apis/signup`,
     API_LOGIN: `${BACKEND}/apis/login`,
     API_DETAIL: `${BACKEND}/apis/get_book`,
     API_ADDBOOK: `${BACKEND}/apis/insert_book`,
     API_DELBOOK: `${BACKEND}/apis/delete_book`, 
     API_TRANSACTION: `${BACKEND}/apis/transactions`,
-    API_AUTH: `${BACKEND}/apis/authentication`,
- 
  
 
 
@@ -32,7 +32,7 @@ export const APP_CONSTANTS = {
     GUEST_ROLE: "guest",
     PERMISSIONS_MAP: {
         user: [],
-        guest: [APP_PATH + "/login.html",APP_PATH + "/detail.html", $$.MONKSHU_CONSTANTS.ERROR_THTML]
+        guest: [APP_PATH + "/login.html",APP_PATH + "/signup.html",APP_PATH + "/detail.html", $$.MONKSHU_CONSTANTS.ERROR_THTML]
     },
     API_KEYS: { "*": "uiTmv5YBOZMqdTb0gekD40PnoxtB9Q0k" },
     KEY_HEADER: "X-API-Key"
